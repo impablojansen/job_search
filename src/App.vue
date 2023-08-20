801d53b52f80f3368bcfe4cf610c9bc9ed72b730
Pablo Jansen <impablojansen@gmail.com>
2023-08-20 20:18:57 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import MainNav from '@/components/MainNav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainNav
+  }
+}
+</script>
+
+<template>
+  <main-nav />
+</template>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'Job Careers'
+    }
+  }
+}
+</script>
+
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 w-full h-16 bg-white">
+      <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
+        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+      </div>
+    </div>
+  </header>
+</template>

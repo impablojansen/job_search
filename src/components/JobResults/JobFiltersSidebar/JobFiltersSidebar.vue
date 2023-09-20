02685c4ddb910173eddbf3eb8c69536bac3e5ccd
Pablo Jansen <impablojansen@gmail.com>
2023-09-19 21:25:40 -0300

    Fifth commit

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -0,0 +1,24 @@
+<script>
+import ActionButton from '@/components/Shared/ActionButton.vue'
+import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
+import JobFiltersSidebarOrganizations from '@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue'
+
+export default {
+  name: 'JobFiltersSidebar',
+  components: { ActionButton, CollapsibleAccordion, JobFiltersSidebarOrganizations }
+}
+</script>
+<template>
+  <div class="flex flex-col border-r border-solid border-brand-gray-1 bg-white p-4 w-96">
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear Filters" type="secondary" />
+        </div>
+      </div>
+
+      <job-filters-sidebar-organizations />
+    </section>
+  </div>
+</template>
--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,16 +1,19 @@
 <script>
+import JobFiltersSidebar from '@/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue'
+import JobListings from '@/components/JobResults/JobListings.vue'
+
 export default {
   name: 'JobResultsView',
-  methods: {
-    goToHome() {
-      this.$router.push({
-        name: 'Home'
-      })
-    }
+  components: {
+    JobFiltersSidebar,
+    JobListings
   }
 }
 </script>
+
 <template>
-  <h1 class="text-6xl font-bold">Job results page</h1>
-  <button @click="goToHome">Go to home page</button>
+  <div class="flex w-full flex-row flex-nowrap">
+    <job-filters-sidebar />
+    <job-listings />
+  </div>
 </template>
